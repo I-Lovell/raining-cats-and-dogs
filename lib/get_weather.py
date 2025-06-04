a414ecd23aa1dacc5b794808905b12490ae82d33
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-06-04 12:32:06 +0100

    created function for London 7 day forecast

---
+++ lib/get_weather.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+import json
+
+# Get and return weather codes, max and min temperatures 
+# and precipitation probability for the next 7 days in London, UK
+def get_weather():
+    url = urlopen("https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Europe%2FLondon")
+    response = url.read().decode('UTF-8')
+    json_data = json.loads(response)
+    return json_data['daily']
