6ef571137c92f699f943c8ed469860abe4e4b0af
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-06-04 16:08:06 +0100

    basic flask app setup

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import os
+from flask import Flask
+from lib.forecast_repository import ForecastRepository
+from lib.forecast import Forecast
+from lib.get_weather import get_weather
 
+# Create flask app (new instance of flask class)
+app = Flask(__name__)
+
+# ---------- Routes Start ----------
+
+@app.route('/forecast', methods=['GET'])
+def get_basic_forecast():
+    data = get_weather()
+    new_forecast = Forecast(data)
+    repository = ForecastRepository()
+    result = repository.sort_forecast_into_days(new_forecast)
+    return result
+
+
+# ---------- Routes Finish ---------
+
+
+# Starts server if app.py is run directly
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
--- lib/get_weather.py
+++ lib/get_weather.py
@@ -7,4 +7,5 @@ def get_weather():
     url = urlopen("https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=Europe%2FLondon")
     response = url.read().decode('UTF-8')
     json_data = json.loads(response)
-    return json_data['daily']
+    next_seven_days = json_data['daily']
+    return next_seven_days
