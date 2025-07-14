23942ccd459b2f61f501f0b078131bb9e4713b61
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-07-14 16:09:16 +0100

    created basic database connection class

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -1,3 +1,22 @@
 import os
 import psycopg
 
+
+class DatabaseConnection:
+    def __init__(self):
+        self.dev = os.getenv("DEV_DATABASE")
+        self.test = os.getenv("TEST_DATABASE")
+
+    def connect(self, test_mode=False):
+        try:
+            if not test_mode:
+                self.connection = psycopg.connect(self.dev)
+            else:
+                self.connection = psycopg.connect(self.test)
+        except psycopg.OperationalError:
+            raise Exception("database connection failed")
+    
+    def seed(self, sql_file):
+        with self.connection.cursor() as cursor:
+            cursor.execute(open(sql_file, "r").read())
+            self.connection.commit
