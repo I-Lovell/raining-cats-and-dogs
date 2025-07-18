803a084c07f315fbc78451f5d7da318b175c264a
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-07-18 11:10:39 +0100

    database connection files and tests

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -1,18 +1,20 @@
 import os
 import psycopg
+from psycopg.rows import dict_row
 
 
 class DatabaseConnection:
-    def __init__(self):
-        self.dev = os.getenv("DEV_DATABASE")
-        self.test = os.getenv("TEST_DATABASE")
+    def __init__(self, test_mode=False):
+        self._dev = os.getenv("DEV_DATABASE")
+        self._test = os.getenv("TEST_DATABASE")
+        self.test_mode = test_mode
 
-    def connect(self, test_mode=False):
+    def connect(self):
         try:
-            if not test_mode:
-                self.connection = psycopg.connect(self.dev)
+            if not self.test_mode:
+                self.connection = psycopg.connect(dbname = self._dev, row_factory=dict_row)
             else:
-                self.connection = psycopg.connect(self.test)
+                self.connection = psycopg.connect(dbname = self._test, row_factory=dict_row)
         except psycopg.OperationalError:
             raise Exception("database connection failed")
     
@@ -20,3 +22,10 @@ class DatabaseConnection:
         with self.connection.cursor() as cursor:
             cursor.execute(open(sql_file, "r").read())
             self.connection.commit
+
+    def execute(self, query, params=[]):
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, params)
+            result = cursor.fetchall()
+            return result
+        
