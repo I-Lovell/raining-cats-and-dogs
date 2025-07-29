b1c4c02e165d5554b182b5dca152c888f786cb9e
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-07-29 17:31:22 +0100

    updated host for CI

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def get_about_page():
 
 # Starts server if app.py is run directly
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5001)))
