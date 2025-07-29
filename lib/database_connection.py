2d865de12f957e9c58ad54569e48ae0ee8986512
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-07-29 16:47:48 +0100

    updated connection logic

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -14,7 +14,7 @@ class DatabaseConnection:
             if not self.test_mode:
                 self.connection = psycopg.connect(dbname = self._dev, row_factory=dict_row)
             else:
-                self.connection = psycopg.connect(dbname = self._test, row_factory=dict_row)
+                self.connection = psycopg.connect(dbname = self._test, row_factory=dict_row, user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
         except psycopg.OperationalError:
             raise Exception("database connection failed")
     
